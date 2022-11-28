2525b1b2556222d6f510a54ac49e861ae3cb4cba
Kabir <kabir.bissessar@aalto.fi>
2022-11-28 17:25:25 +0200

    gui displays moving shapes

---
+++ build/CMakeFiles/tower-defense.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tower-defense.dir/src/gui.cpp.o"
+  "CMakeFiles/tower-defense.dir/src/main.cpp.o"
+  "tower-defense"
+  "tower-defense.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tower-defense.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
