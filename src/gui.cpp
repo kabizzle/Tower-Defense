216916475ced35836c5cccdc1b05d9c87be171e6
peltoke1 <elias.peltokangas@aalto.fi>
2022-12-07 14:34:46 +0200

    Small fix to gui.cpp

--- src/gui.cpp
+++ src/gui.cpp
@@ -1,18 +1,15 @@
-#include <iostream>
 #include "gui.hpp"
+
+#include <iostream>
+
 #include "button.hpp"
 
-
-GUI::GUI() :
-  m_renderables(new Renderables()) {
-    
-  }
+GUI::GUI() : m_renderables(new Renderables()) {}
 
 GUI::~GUI() {
   delete this->m_window;
   delete this->m_renderables;
 }
-
 
 void GUI::init() {
   this->m_window = nullptr;
@@ -21,32 +18,25 @@ void GUI::init() {
   this->start = sf::Vector2f(100, 200);
 }
 
-
-bool GUI::running() {
-  return this->m_window->isOpen();
-}
-
+bool GUI::running() { return this->m_window->isOpen(); }
 
 void GUI::pollEvents() {
   while (this->m_window->pollEvent(this->m_event)) {
-    if (this->m_event.type == sf::Event::Closed)
-      this->m_window->close();
-    
-    if (this->m_event.type == sf::Event::MouseButtonPressed)
-{
-    if (this->m_event.mouseButton.button == sf::Mouse::Right)
-    {
+    if (this->m_event.type == sf::Event::Closed) this->m_window->close();
+
+    if (this->m_event.type == sf::Event::MouseButtonPressed) {
+      if (this->m_event.mouseButton.button == sf::Mouse::Right) {
         std::cout << "the right button was pressed" << std::endl;
         std::cout << "mouse x: " << this->m_event.mouseButton.x << std::endl;
         std::cout << "mouse y: " << this->m_event.mouseButton.y << std::endl;
+      }
     }
-}
   }
 }
 
-
 void GUI::render() {
-  this->m_window = new sf::RenderWindow(this->m_videoMode, "test window", sf::Style::Close);
+  this->m_window =
+      new sf::RenderWindow(this->m_videoMode, "test window", sf::Style::Close);
   this->m_window->setFramerateLimit(60);
   // this->addEnemy("bachelor_2", this->start);
   // sf::Vector2f pos = this->start;
@@ -55,10 +45,10 @@ void GUI::render() {
 }
 
 // void GUI::addEnemy(std::string type, sf::Vector2f pos) {
-  
+
 //   // if (this->textures_map.find(type) != this->textures_map.end()){
 //   //   if (!this->texture.loadFromFile(this->textures_map[type])) {
-//   //     std::cout << "Error: Image not found" << std::endl; 
+//   //     std::cout << "Error: Image not found" << std::endl;
 //   //     return;
 //   //   }
 //   //   sf::Sprite enemy(this->texture);
@@ -73,9 +63,10 @@ void GUI::render() {
 // }
 
 void GUI::moveEnemies() {
-  for (auto enemy = this->enemies.begin(); enemy != this->enemies.end(); enemy++){
+  for (auto enemy = this->enemies.begin(); enemy != this->enemies.end();
+       enemy++) {
     sf::Vector2f position = enemy->getPosition();
-    if (position.x < 0 || position.x > 1280 - 20) this->x_velo *= -1; 
+    if (position.x < 0 || position.x > 1280 - 20) this->x_velo *= -1;
     // if (position.y < 0 || position.y > 720 - 20) this->y_velo *= -1;
     position.x += this->x_velo;
     // position.y += this->y_velo;
@@ -84,15 +75,12 @@ void GUI::moveEnemies() {
   }
 }
 
-
-Button* createButton(std::string text, int x, int y) {
+Button* GUI::createButton(std::string text, int x, int y) {
   Button* button = new Button(text, x, y);
   return button;
 }
 
-
 void GUI::update() {
-
   this->pollEvents();
 
   // clear window
@@ -100,11 +88,11 @@ void GUI::update() {
 
   // draw changes
   this->m_window->draw(m_renderables->getBackgroundSprite());
-    // this->moveEnemies();
-    for (auto enemy = this->enemies.begin(); enemy != this->enemies.end(); enemy++){
-      this->m_window->draw(*enemy);
-    }
-
+  // this->moveEnemies();
+  for (auto enemy = this->enemies.begin(); enemy != this->enemies.end();
+       enemy++) {
+    this->m_window->draw(*enemy);
+  }
 
   // display window
   this->m_window->display();
