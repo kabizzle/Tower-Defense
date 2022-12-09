da10f7a5b0a0eb1c1176bda23a3024cfd944291e
Kabir <kabir.bissessar@aalto.fi>
2022-12-09 19:37:27 +0200

    Updated upgrade tower button

--- src/states/gamestate.cpp
+++ src/states/gamestate.cpp
@@ -279,7 +279,7 @@ void GameState::Draw() {
                                        static_cast<Action>(i))) {
         if (i == Action::UpgradeTower){
           const AttackingTower* tower = static_cast<const AttackingTower*>(m_gameLogic.GetTower({m_selX, m_selY}));
-          m_buttons[Action::UpgradeTower]->changeText("Upgrade tower ( " + std::to_string(tower->GetUpgradeCost()) + ")");
+          m_buttons[Action::UpgradeTower]->changeText("Upgrade tower" + std::to_string(tower->GetUpgradeCost()) + "cr");
         }
         button->enableButton();
       } else {
