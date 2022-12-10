2ad8846769a967e2a934b497a98dc289b70b3d62
Juho_P <juho.poteri@aalto.fi>
2022-12-10 15:27:51 +0200

    Utils comments

--- src/utils.hpp
+++ src/utils.hpp
@@ -6,9 +6,14 @@
 
 /**
  * @brief A namespace containing some utility functions needed in the project
- *
  */
 namespace UtilFunctions {
-float distance(const std::pair<int32_t, int32_t>& c1,
-               const std::pair<int32_t, int32_t>& c2);
+  /**
+   * @brief Calculates the Euclidean distance between two coordinate pairds
+   * @param c1 coordinate 1
+   * @param c2 coordinate 2
+   * @return float 
+   */
+  float distance(const std::pair<int32_t, int32_t>& c1,
+                 const std::pair<int32_t, int32_t>& c2);
 }  // namespace UtilFunctions
